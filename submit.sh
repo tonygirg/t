a07a0387477f9c25510933f750f573c05cea3749
William E. Skeith <wes@cs.ccny.cuny.edu>
2022-02-08 20:23:23 -0500

    Initial commit.
    Adds submission script.

---
+++ submit.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# TODO: make sure you edit postdata to reflect your information.
+. ./postdata
+if (( $# != 1 )) ; then
+	echo "Usage: $0 <filename>"
+	exit 1
+fi
+
+rcpt=$(basename $1)-rcpt.html || exit 127
+sha1=$(sha1sum $1 | head -c40) || exit 127
+
+curl -k --form "file=@$1" --form lname=$lastname --form sid=$sidlast4 \
+	"https://www-cs.ccny.cuny.edu/~wes/CSC212/upload_file.php" > $rcpt
+
+if (( $? != 0 )) ; then
+	echo -e "\033[31mError:\033[0m upload failed.  Check network?"
+	exit 1
+fi
+if grep -qi 'path not found' $rcpt ; then
+	echo -e "\033[31mError:\033[0m Upload path not found."
+	echo "Double check 'postdata' contents; make sure you are on the roster."
+	exit 1
+fi
+if grep -qiE '(fail|warn)' $rcpt ; then
+	echo -e "\033[1;31mWarning:\033[0m Receipt looks wrong.  Double check postdata?"
+	exit 2
+fi
+if grep -qiE "${sha1}" $rcpt ; then
+	echo -e "\033[32mSuccess (really!).\033[0m  See receipt in $rcpt"
+	exit 0
+fi
+echo -e "\033[1;31mWarning:\033[0m Couldn't find hash in receipt.  Consult prof..."
+exit 2
